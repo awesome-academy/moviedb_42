048221f0cbbd94316dc6c75c8b306fc42d3b443b
Duong Minh Hoang <=>
2019-05-10 09:58:17 +0700

    Desing ui and binding genres fragment

--- app/src/main/java/com/sun/moviesun/ui/home/MainPageAdapter.kt
+++ app/src/main/java/com/sun/moviesun/ui/home/MainPageAdapter.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.sun.moviesun.data.annotation.PageKeyDef
 import com.sun.moviesun.ui.home.discover.DiscoverFragment
+import com.sun.moviesun.ui.home.genres.GenresFragment
 
 class MainPageAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
 
   override fun getItem(position: Int): Fragment =
       when (position) {
         PageKeyDef.DISCOVER -> { DiscoverFragment.newInstance() }
+        PageKeyDef.GENRE -> { GenresFragment.newInstance() }
         else -> { DiscoverFragment.newInstance() }
       }
 
--- app/src/main/java/com/sun/moviesun/ui/home/genres/GenresFragment.kt
+++ app/src/main/java/com/sun/moviesun/ui/home/genres/GenresFragment.kt
@@ -4,14 +4,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.sun.moviesun.R
+import com.sun.moviesun.databinding.GenresFragmentBinding
+import com.sun.moviesun.util.extension.provideMovieRepository
 
 class GenresFragment : Fragment() {
 
+  private lateinit var genresBinding: GenresFragmentBinding
+  private lateinit var genresViewModel: GenresViewModel
+
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                             savedInstanceState: Bundle?): View? {
-    return inflater.inflate(R.layout.genres_fragment, container, false)
+    genresViewModel = GenresViewModel(activity!!.applicationContext.provideMovieRepository(), childFragmentManager)
+    genresBinding = DataBindingUtil.inflate(inflater, R.layout.genres_fragment, container, false)
+    initializeUI()
+    return genresBinding.root
+  }
+
+  private fun initializeUI() {
+    genresBinding.run {
+      viewModel = genresViewModel
+      tabLayout.setupWithViewPager(pagerGenre, true)
+    }
+  }
+
+  override fun onStop() {
+    super.onStop()
+    genresViewModel.onCleared()
   }
 
   companion object {
